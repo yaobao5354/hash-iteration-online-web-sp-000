22fe24694e3dce23206de56174d62ce8dd3bf14b
yaoyu bao <yaobao5354@gmail.com>
2019-08-25 15:38:37 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,7 +10,6 @@ def happy_birthday(birthday_kids)
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
-end
 
 #def age_appropriate_birthday(birthday_kids)
  # birthday_kids.each do |kids_name, age|
